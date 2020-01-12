eb80304c32dada488e88522ab17b445c46fd534a
vst <vst@lindenbox.de>
2020-01-12 18:15:20 +0100

    Added installation routine for zsh-syntax-highlighing plugin on macos

--- setup.sh
+++ setup.sh
@@ -11,15 +11,34 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
-	echo "cloning powerlevel9k-theme..."
+	echo "* cloning powerlevel9k-theme..."
 	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+else echo "* powerlevel9k-theme already installed"
 fi
 
 ### Install zsh syntax highlighting plugin:
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+	echo "* cloning zsh-syntax highlighting plugin from git repo"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else echo "* zsh-syntax highlighting plugin alredy installed" 
+fi
 
 if [ -e ~/.zshrc ];then 
 	rm -f ~/.zshrc;
 fi
 
 ln -s ~/.zsh/zshrc ~/.zshrc
+
+
+if [ `which port` ]; then
+	if [ `port list py-pygments | wc -l` -eq 0 ]; then
+		echo "* Installing colorize dependencies by port"
+		sudo port install py-pygments
+		## Activate pygments for the right python version
+		PYTHON_VERSION=$(python --version | sed 's/\.//g' | cut -c 8-9)
+		set -x
+		sudo port select --set pygments py${PYTHON_VERSION}-pygments
+		set +x
+	else echo "* colorize plugin dependencies already installed"
+	fi
+fi
